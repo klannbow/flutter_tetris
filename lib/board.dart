d6616b10ff57e739766f2fc5f739d8ddf420f13e
klannbow <klannbow@gmail.com>
2024-09-12 01:12:13 +0900

    完成

--- lib/board.dart
+++ lib/board.dart
@@ -1,7 +1,21 @@
+import 'dart:async';
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:tetris/piece.dart';
 import 'package:tetris/pixel.dart';
 import 'package:tetris/value.dart';
+
+/*
+ GAME BOARD
+
+ this is a 2x2 grid with null representing an empty space.
+ A non empty space will have the color to represent the landed pieces
+ */
+
+// create game board
+List<List<Tetromino?>> gameBoard =
+    List.generate(colLength, (i) => List.generate(rowLength, (j) => null));
 
 class GameBoard extends StatefulWidget {
   const GameBoard({super.key});
@@ -11,12 +25,14 @@ class GameBoard extends StatefulWidget {
 }
 
 class _GameBoardState extends State<GameBoard> {
-  // grid dimensions
-  int rowLength = 10;
-  int colLength = 15;
-
   // current tetris piece
-  Piece currentPiece = Piece(type: Tetromino.L);
+  Piece currentPiece = Piece(type: Tetromino.Z);
+
+  // current store
+  int currentScore = 0;
+
+  // game over status
+  bool gameOver = false;
 
   @override
   void initState() {
@@ -26,31 +42,279 @@ class _GameBoardState extends State<GameBoard> {
     startGame();
   }
 
+  // clear lines
+  void clearLines() {
+    // step 1: Loop through each row of the game board from bottom to top
+    for (int row = colLength - 1; row >= 0; row--) {
+      // step 2: Initialize a variable to track if the row is full
+      bool rowIsFull = true;
+
+      // step 3: Check if the row if full (all columns in the row are filled with pieces)
+      for (int col = 0; col < rowLength; col++) {
+        // if there's an empty column, set rowIsFull to false and break the loop
+        if (gameBoard[row][col] == null) {
+          rowIsFull = false;
+          break;
+        }
+      }
+
+      // step 4: if the row is full, clear the row and shift rows down
+      if (rowIsFull) {
+        {
+          // step 5: move all rows above the cleared row down by one position
+          for (int r = row; r > 0; r--) {
+            // copy the above row to the current row
+            gameBoard[r] = List.from(gameBoard[r - 1]);
+          }
+
+          // step 6: set the top row to empty
+          gameBoard[0] = List.generate(row, (index) => null);
+
+          // step 7: Increase the score:
+          currentScore++;
+        }
+      }
+    }
+  }
+
+  // GAME OVER METHOD
+  bool isGameOver() {
+    // check if any coloums in the top row are filled
+    for (int col = 0; col < rowLength; col++) {
+      if (gameBoard[0][col] != null) {
+        return true;
+      }
+    }
+    return false;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.black,
-      body: GridView.builder(
-          itemCount: rowLength * colLength,
-          physics: const NeverScrollableScrollPhysics(),
-          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: rowLength),
-          itemBuilder: (context, index) {
-            if (currentPiece.position.contains(index)) {
-              return Pixel(
-                color: Colors.yellow,
-                child: index,
-              );
-            } else {
-              return Pixel(
-                color: Colors.grey[900],
-                child: index,
-              );
-            }
-          }),
+      body: Column(
+        children: [
+          Expanded(
+            // GAME BOARD
+            child: GridView.builder(
+                itemCount: rowLength * colLength,
+                physics: const NeverScrollableScrollPhysics(),
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: rowLength),
+                itemBuilder: (context, index) {
+                  // get row and col of each index
+                  int row = (index / rowLength).floor();
+                  int col = index % rowLength;
+
+                  // current piece
+                  if (currentPiece.position.contains(index)) {
+                    return Pixel(
+                      color: currentPiece.color,
+                      child: index,
+                    );
+                    // landed pieces
+                  } else if (gameBoard[row][col] != null) {
+                    final Tetromino? tetrominoType = gameBoard[row][col];
+                    return Pixel(
+                        color: tetrominoColors[tetrominoType], child: '');
+                  }
+                  // blank pixel
+                  else {
+                    return Pixel(
+                      color: Colors.grey[900],
+                      child: index,
+                    );
+                  }
+                }),
+          ),
+
+          // SCORE
+          Text(
+            'Score: $currentScore',
+            style: const TextStyle(color: Colors.white),
+          ),
+
+          // GAME CONTROLS
+          Padding(
+            padding: const EdgeInsets.only(bottom: 50.0, top: 50),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                // left
+                IconButton(
+                    onPressed: moveLeft,
+                    color: Colors.white,
+                    icon: Icon(Icons.arrow_back_ios)),
+                // rotate
+                IconButton(
+                    onPressed: rotatePiece,
+                    color: Colors.white,
+                    icon: Icon(Icons.rotate_right)),
+                // right
+                IconButton(
+                    onPressed: moveRight,
+                    color: Colors.white,
+                    icon: Icon(Icons.arrow_forward_ios)),
+              ],
+            ),
+          ),
+        ],
+      ),
     );
   }
 
   void startGame() {
     currentPiece.initializePiece();
+
+    // frame refresh rate
+    Duration frameRate = const Duration(milliseconds: 200);
+    gameLoop(frameRate);
+  }
+
+  // game loop
+  void gameLoop(Duration frameRate) {
+    Timer.periodic(frameRate, (timer) {
+      setState(() {
+        // clear lines
+        clearLines();
+        // check landing
+        checkLanding();
+        // check if game is over
+        if (gameOver) {
+          timer.cancel();
+          showGameOverDialog();
+        }
+        // move current piece down.
+        currentPiece.movePiece(Direction.down);
+      });
+    });
+  }
+
+  // check for collision in a future position
+  // return true -> there is a collision
+  // return false -> there is no collision
+  bool checkCollision(Direction direction) {
+    // loop through each position of the current piece
+    for (int i = 0; i < currentPiece.position.length; i++) {
+      // calculate the row and column of the current position
+      int row = (currentPiece.position[i] / rowLength).floor();
+      int col = currentPiece.position[i] % rowLength;
+
+// adjust the row and col based on the direction
+      if (direction == Direction.left) {
+        col -= 1;
+      } else if (direction == Direction.right) {
+        col += 1;
+      } else if (direction == Direction.down) {
+        row += 1;
+      }
+// check if the piece is out of bounds (either too low of too far to the left or right)
+      if (row >= colLength || col < 0 || col >= rowLength) {
+        return true;
+      }
+      if (row >= 0 && gameBoard[row][col] != null) {
+        return true;
+      }
+    }
+    // if no collisions are ditected, return false
+    return false;
+  }
+
+  void checkLanding() {
+    // if going down is occupied
+    if (checkCollision(Direction.down)) {
+      // mark position as occupied on the gameboard
+      for (int i = 0; i < currentPiece.position.length; i++) {
+        int row = (currentPiece.position[i] / rowLength).floor();
+        int col = currentPiece.position[i] % rowLength;
+        if (row >= 0 && col >= 0) {
+          gameBoard[row][col] = currentPiece.type;
+        }
+      }
+
+      // once landed, create the next piece
+      createNewPiece();
+    }
+  }
+
+  void createNewPiece() {
+    // create a random object to generate random tetrimino types
+    Random rand = Random();
+
+    // create a new piece with random type
+    Tetromino randomType =
+        Tetromino.values[rand.nextInt(Tetromino.values.length)];
+    currentPiece = Piece(type: randomType);
+    currentPiece.initializePiece();
+
+    /*
+    Since our game over condition is if there is a piece at the top level,
+    you want to check if the game is over when you create a new piece
+    instead of checking every frame, because new pieces are allowed to go through the top level
+    but if there is already a piece in the top level when the new piece is created,
+    then game is over.
+     */
+    if (isGameOver()) {
+      gameOver = true;
+    }
+  }
+
+  // move left
+  void moveLeft() {
+    // make sure the move is valid befor moving there
+    if (!checkCollision(Direction.left)) {
+      currentPiece.movePiece(Direction.left);
+    }
+  }
+
+  // move right
+  void moveRight() {
+    // make sure the move is valid befor moving there
+    if (!checkCollision(Direction.right)) {
+      currentPiece.movePiece(Direction.right);
+    }
+  }
+
+  // rotate piece
+  void rotatePiece() {
+    setState(() {
+      currentPiece.rotatePiece();
+    });
+  }
+
+  // game over message
+  void showGameOverDialog() {
+    showDialog(
+        context: context,
+        builder: (context) => AlertDialog(
+              title: Text('Game Over'),
+              content: Text("Your score is: $currentScore"),
+              actions: [
+                TextButton(
+                    onPressed: () {
+                      // reset the game
+                      resetGame();
+                      Navigator.pop(context);
+                    },
+                    child: Text('Play Again'))
+              ],
+            ));
+  }
+
+  // reset game
+  void resetGame() {
+    // clear the game board
+    gameBoard =
+        List.generate(colLength, (i) => List.generate(rowLength, (j) => null));
+
+    // new game
+    gameOver = false;
+    currentScore = 0;
+
+    // create new piece
+    createNewPiece();
+
+    // start game again
+    startGame();
   }
 }
--- lib/value.dart
+++ lib/value.dart
@@ -1,3 +1,9 @@
+// grid dimensions
+import 'package:flutter/material.dart';
+
+int rowLength = 10;
+int colLength = 15;
+
 enum Direction {
   left,
   right,
@@ -13,3 +19,13 @@ enum Tetromino {
   Z,
   T,
 }
+
+const Map<Tetromino, Color> tetrominoColors = {
+  Tetromino.L: Color(0xFFFFA500), // Orange
+  Tetromino.J: Color.fromARGB(255, 0, 102, 255), // Blue
+  Tetromino.I: Color.fromARGB(255, 242, 0, 255), // Pink
+  Tetromino.O: Color(0xFFFFFF00), // Yellow
+  Tetromino.S: Color(0xFF008000), // Green
+  Tetromino.Z: Color(0xFFFF0000), // Red
+  Tetromino.T: Color.fromARGB(255, 144, 0, 255), // Purple
+};
